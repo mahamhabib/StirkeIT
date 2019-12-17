0939f105c7a93bf7aa93963d0283190ae095cae7
maha.simba <maha.moustafa.habib@gmail.com>
2019-12-17 11:16:20 +0100

    Initial Commit

---
+++ StirkeIT/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StirkeIT
+//
+//  Created by Maha Habib on 17/12/2019.
+//  Copyright © 2019 Maha Habib. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
